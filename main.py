c940cb6059f09d02572afbd3cfc24f4711d7bee4
nangell18 <nate@om-bi.com>
2025-07-14 08:07:57 -0400

    set up environment and look at main.py file
    this is working code that allows you to automatically create 15 qr codes for the specific url we give it in the prompt. BE CAREFUL: this code is overly permissive and if an hacker attacks our system he could put dangerous code

---
+++ main.py
@@ -0,0 +1,66 @@
+from dotenv import load_dotenv
+from langchain import hub
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain_experimental.tools import (
+    PythonREPLTool,
+)  # this is a dangerous tool because it gives our llm an ability to write and execute python code in the interpreter
+
+load_dotenv()  # load all of our .env files to our environment
+
+
+# ---------------
+
+"""I DID NOT RUN THIS CODE BECAUSE OF THIS REASON ->
+And please, please, please, be careful when you use those type of tools.
+This is an overly permissive tool that can do a lot of things. This is literally remote code execution.
+So, if an attacker has access to our agent and is able to input some prompts to it,
+then it's basically has the power to run code in our systems.
+So, we should be very, very careful when we are using those type of tools."""
+
+"""it is also important that we already have qrcode package already installed in our local
+environment. I DO NOT KNOW if that it would work without it but it is an important note to remember"""
+
+# ----------------
+
+
+def main():
+    print("start....")
+
+    instructions = """You are an agent designed to write and execute python code to answer questions.
+        You have access to a python REPL, which you can use to execute python code.
+        If you get an error, debug your code and try again.
+        Only use the output of your code to answer the question. 
+        You might know the answer without running any code, but you should still run the code to get the answer.
+        If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
+        """
+
+    base_prompt = hub.pull("langchain-ai/react-agent-template")
+    prompt = base_prompt.partial(instructions=instructions)
+
+    tools = [
+        PythonREPLTool()
+    ]  # so we want to create our tool list, and we want our tool list to be the Python REPL tool. The Python REPL tool is a Python shell that can execute Python commands and Python code. So, its input should be valid Python code.
+    # we do need to write any tool description, tool name because it is already a tool object
+
+    agent = create_react_agent(
+        prompt=prompt,
+        llm=ChatOpenAI(
+            temperature=0, model="gpt-4-turbo"
+        ),  # we needed this gpt-4 because the reasoning engine is much better. we need reasoning engine
+        # however, it is slower and more expensive.
+        tools=tools,
+    )
+
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+    agent_executor.invoke(
+        input={
+            "input": """generate and save in current working directory 15 QRcodes
+                                    that point to www.udemy.com/course/langchain, you have qrcode package installed already"""
+        }
+    )
+
+
+if __name__ == "__main__":
+    main()
